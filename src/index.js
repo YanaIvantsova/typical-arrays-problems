1f74b1e1044905c277c346c96763eb2bc6972cf7
yanaivantsova <yanaivantsova@yandex.by>
2023-06-06 16:40:00 +0200

    feat: add task decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array == undefined || array.length === 0 ? 0 : array.reduce((a, b) => a > b ? b : a);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array == undefined || array.length === 0 ? 0 : array.reduce((a, b) => a > b ? a : b);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return array == undefined || (array.length === 0) ? 0 : array.reduce((a, b) => a + b) / array.length;
 }
